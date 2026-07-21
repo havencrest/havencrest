86a5e6a64a248e2c56c302afe399c122aa917245
kimaniimmanuel <immanuelmkimani@gmail.com>
2026-07-21 14:43:32 +0300

    Added ReadME

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,18 +1,7 @@
-<!-- <template>
-  <h1 class="text-3xl font-bold text-amber-500">You did it!</h1>
-  <p>
-    Visit
-    <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
-</template>
-
-<script setup></script> -->
-
 <template>
   <div class="flex min-h-screen flex-col bg-white text-brand-ink">
     <AppHeader />
-    <main class="flex-1">
+    <main class="flex-1 mt-30">
       <RouterView />
     </main>
     <AppFooter />
